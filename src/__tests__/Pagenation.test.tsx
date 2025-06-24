20c6326128fba5d2edc9f7ef617eaf4960115abf
LeeJaeHo99 <ljh2735294@naver.com>
2025-06-24 23:56:34 +0900

    Jest + RTL

---
+++ src/__tests__/Pagenation.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { useState } from "react";
+import Pagination from "@/components/board/Pagination";
+
+function TestPagination() {
+    const [pagination, setPagination] = useState(0);
+
+    const plusPagination = () => {
+        if (pagination + 1 === 10) return;
+        setPagination(pagination + 1);
+    };
+    const minusPagination = () => {
+        if (pagination === 0) return;
+        setPagination(pagination - 1);
+    };
+
+    return (
+        <Pagination
+            data={[]}
+            pagination={pagination}
+            setPagination={setPagination}
+        />
+    );
+}
+
+describe("Pagination 컴포넌트", () => {
+    it("pagination 버튼이 정상적으로 작동한다.", () => {
+        render(<TestPagination />);
+
+        
+    });
+});

44fd436e99574f83b8204db039fc76d89a1a2543
LeeJaeHo99 <ljh2735294@naver.com>
2025-07-03 11:57:52 +0900

    메타 데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,30 +5,49 @@ import Header from "@/components/ui/Header";
 
 export const metadata: Metadata = {
     title: "최애의 전당",
-    description: "당신의 최애를 우승시켜라!",
+    description:
+        "나의 최애를 1등으로!",
+    authors: [{ name: "이재호", url: "https://www.myidolranking.com" }],
+    creator: "이재호",
+    publisher: "이재호",
     openGraph: {
-        title: "최애의 전당",
-        description: "당신의 최애를 우승시켜라!",
-        url: "https://your-domain.com/about",
+        type: "website",
         siteName: "최애의 전당",
+        locale: "ko_KR",
+        title: "최애의 전당",
+        description:
+            "나의 최애를 1등으로!",
+        url: "https://www.myidolranking.com",
         images: [
             {
-                url: "https://your-domain.com/og-image.png",
+                url: "/images/bg.png",
                 width: 1200,
                 height: 630,
+                alt: "최애의 전당",
             },
         ],
-        locale: "ko_KR",
-        type: "website",
     },
     icons: {
-        icon: "/icons/favicon.png",
+        icon: "/icons/favicon.ico",
+        shortcut: "/icons/favicon.ico",
+        apple: "/icons/favicon.ico",
     },
+    themeColor: "#ffffff",
     twitter: {
         card: "summary_large_image",
         title: "최애의 전당",
-        description: "당신의 최애를 우승시켜라!",
-        images: ["https://your-domain.com/og-image.png"],
+        description: "나의 최애를 1등으로!",
+        images: ["/images/bg.png"],
+    },
+    robots: {
+        index: true,
+        follow: true,
+        nocache: false,
+        googleBot: {
+            index: true,
+            follow: true,
+            noimageindex: false,
+        },
     },
 };
 
